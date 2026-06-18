ceb038705c2a166defccbd93c212f00c2ec7b501
Emanuel Leão <emanuelleao611@gmail.com>
2026-06-18 18:27:21 -0300

    Clean up unused imports in database.py
    Remove unused imports related to MongoDB and bcrypt.

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 #import os
 #from pymongo import MongoClient
-#import bcrypt
+
 
 #mongo = MongoClient(os.getenv("MONGO_URL", "mongodb://localhost:port"))
 #db = mongo[os.getenv("DB_NAME", "name_table")]
